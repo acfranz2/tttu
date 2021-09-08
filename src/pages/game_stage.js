1da4b66587b73fed46162136e8312d8a388eaef5
acfranz2 <acfranz2@gmail.com>
2021-09-08 14:53:22 -0500

    instructions

--- src/pages/game_stage.js
+++ src/pages/game_stage.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import Navbar from '../components/Navbar'
 import L2Game from '../l2Game.js'
 import Game from './game.js'
+import Instructions from './instructions.js'
 
 export default function game_stage() {
   return (
     <div>
       <Navbar />
-      <Game />
+      <Instructions />
     </div>
   );
 }
